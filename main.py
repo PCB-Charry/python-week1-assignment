de553b82cea1fb17bec2c462f5a47853a5f43e0b
unknown <charityatieno861@gmail.com>
2025-08-13 19:15:39 +0300

    Add simple two-number calculator with basic arithmetic operations

---
+++ main.py
@@ -0,0 +1,27 @@
+# Get the first number from user input and convert it to a float  
+first_number = float(input("Enter first number: "))  
+ 
+
+# Get the second number from user input and convert it to a float  
+second_number = float(input("Enter second number: "))  
+
+# Perform addition and store the result in 'addition'  
+addition = first_number + second_number  
+# Print the result of addition  
+print("Addition:", addition)  
+
+# Perform multiplication and store the result in 'multiplication'  
+multiplication = first_number * second_number  
+# Print the result of multiplication  
+print("Multiplication:", multiplication)  
+
+# Perform subtraction and store the result in 'subtraction'  
+subtraction = first_number - second_number  
+# Print the result of subtraction  
+print("Subtraction:", subtraction)  
+
+# Perform division and store the result in 'division'  
+division = first_number / second_number 
+ 
+# Print the result of division  
+print("Division:", division)  
